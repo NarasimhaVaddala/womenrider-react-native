0d4191739b0a43a62574fe97532b859e7757521f
NarasimhaVaddala <vaddalanarasimha@gmail.com>
2024-11-01 18:04:05 +0530

    First Commit

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import One from '../components/One'
+
+export default function index() {
+  return (
+    <View>
+      <One/>
+    </View>
+  )
+}
---
+++ components/One.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function One() {
+  return (
+    <View>
+      <Text className="bg-red-400 text-white">
+
+        hi
+
+
+      </Text>
+    </View>
+  )
+}
